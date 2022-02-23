9b82c9a4a5b3cc745ed09814cc5a762b0f6cf130
卢 志豪 <luzhihao@youcheyihou.com>
2022-02-23 21:02:27 +0800

    no message

--- common/mytiqu.py
+++ common/mytiqu.py
@@ -2,7 +2,6 @@ import jsonpath
 
 
 def excel(excel_extract,respose,classDATA):
-    """瓜瓜提醒一定记得先转换为一个字典"""
     excel_extract_dict=eval(excel_extract)
 
     for key,value in excel_extract_dict.items():
